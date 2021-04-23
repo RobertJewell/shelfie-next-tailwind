7082415d9fb32e656ff25f9719be2b9c64194cbf
Robert Jewell <rjewellaudio@gmail.com>
2021-04-23 17:33:59 +0200

    fix navbar hover and sizings

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,16 +1,29 @@
 import React from "react";
 import NavBarMenu from "./NavBarMenu";
+import Image from "next/image";
+import Link from "next/link";
 
 const NavBar = () => {
 	return (
-		<div className="absolute top-0 hidden bg-pink-100 sm:relative sm:block">
-			<div className="container px-6 py-1 mx-auto text-gray-600 sm:px-6 md:px-12 sm:py-4">
-				<div className="flex flex-row justify-between mx-auto font-serif text-sm ">
-					<h1>Logo</h1>
+		<div className="absolute top-0 hidden sm:relative sm:block ">
+			<div className="container px-6 mx-auto font-bold text-gray-600 sm:px-6 md:px-12 ">
+				<div className="flex flex-row items-center justify-between h-16 mx-auto font-serif text-sm ">
+					<Link href="/">
+						<a className="self-start">
+							<div className="h-12 overflow-hidden bg-pink-200 rounded-b-full ">
+								<Image
+									src="/images/logo--navbar.png"
+									alt="Logo"
+									width={96}
+									height={48}
+								></Image>
+							</div>
+						</a>
+					</Link>
 					<ul className="flex flex-row justify-around w-1/2">
 						<NavBarMenu></NavBarMenu>
-						<li className="w-16 text-center">Galeria</li>
-						<li className="w-16 text-center">FAQ</li>
+						<li className="w-24 text-center">Galeria</li>
+						<li className="w-24 text-center">FAQ</li>
 					</ul>
 					<p>Social</p>
 				</div>
--- components/NavBarMenu.js
+++ components/NavBarMenu.js
@@ -17,15 +17,15 @@ const NavBarMenu = () => {
 
 	return (
 		<div
-			className="relative w-16 pb-6 cursor-pointer"
+			className="relative w-24 pb-6 cursor-pointer"
 			onMouseEnter={() => setOpen(true)}
 			onMouseLeave={() => setOpen(false)}
 		>
-			<div className="absolute flex flex-row w-16 text-gray-600 ">
-				<li className="text-center">Menu</li>
+			<div className="absolute flex flex-row justify-center w-24 text-gray-600 ">
+				<li>Menu</li>
 				<svg
 					xmlns="http://www.w3.org/2000/svg"
-					className="h-4 w-4 m-auto pt-0.5 text-gray-600"
+					className="relative w-6 h-4 text-gray-600 top-0.5"
 					fill="none"
 					viewBox="0 0 24 24"
 					stroke="currentColor"
@@ -33,7 +33,7 @@ const NavBarMenu = () => {
 					<path
 						strokeLinecap="round"
 						strokeLinejoin="round"
-						strokeWidth={2}
+						strokeWidth={3}
 						d="M19 9l-7 7-7-7"
 					/>
 				</svg>
