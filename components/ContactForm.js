2d447540ba3321bfe7ea83c733c2ce467bf6f466
Robert Jewell <rjewellaudio@gmail.com>
2021-05-04 16:36:09 +0200

    inital contact from validation

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -1,5 +1,9 @@
 import { motion } from "framer-motion";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as Yup from "yup";
 
+// Framer motion animation variants
 const fadeInUp = {
 	initial: { y: 60, opacity: 0 },
 	animate: {
@@ -9,8 +13,29 @@ const fadeInUp = {
 };
 
 export default function ContactForm() {
+	// form validation rules
+	const validationSchema = Yup.object().shape({
+		name: Yup.string().required("Name is required"),
+		email: Yup.string().required("Email is required").email("Email is invalid"),
+		message: Yup.string().required("Message is required"),
+	});
+	const formOptions = { resolver: yupResolver(validationSchema) };
+
+	// get functions to build form with useForm() hook
+	const { register, handleSubmit, formState } = useForm(formOptions);
+	const { errors } = formState;
+
+	function onSubmit(data) {
+		// display form data on success
+		console.log(JSON.stringify(data));
+		return false;
+	}
+
 	return (
-		<form className="w-full max-w-lg mx-auto mt-12" action="">
+		<form
+			className="w-full max-w-lg mx-auto mt-12"
+			onSubmit={handleSubmit(onSubmit)}
+		>
 			<motion.div className="flex flex-wrap mb-6 -mx-3" variants={fadeInUp}>
 				<div className="w-full px-3 md:mb-0">
 					<label
@@ -20,12 +45,16 @@ export default function ContactForm() {
 						Name
 					</label>
 					<input
-						className="block w-full px-4 py-3 mb-3 leading-tight text-gray-600 bg-white border border-pink-200 appearance-none rounded-xl focus:outline-none focus:bg-white focus:border-pink-500"
+						className={`block w-full px-4 py-3 mb-3 leading-tight text-gray-600 bg-white border border-pink-200 appearance-none rounded-xl focus:outline-none focus:bg-white focus:border-pink-500  ${
+							errors.name ? "is-invalid" : ""
+						}`}
 						id="name"
 						type="name"
 						name="name"
 						placeholder=""
+						{...register("name")}
 					/>
+					<div className="text-red-600 ">{errors.name?.message}</div>
 				</div>
 			</motion.div>
 			<motion.div className="flex flex-wrap mb-6 -mx-3" variants={fadeInUp}>
@@ -37,12 +66,16 @@ export default function ContactForm() {
 						E-mail
 					</label>
 					<input
-						className="block w-full px-4 py-3 mb-3 leading-tight text-gray-600 bg-white border border-pink-200 appearance-none rounded-xl focus:outline-none focus:bg-white focus:border-pink-500"
-						type="email"
+						className={`block w-full px-4 py-3 mb-3 leading-tight text-gray-600 bg-white border border-pink-200 appearance-none rounded-xl focus:outline-none focus:bg-white focus:border-pink-500 ${
+							errors.email ? "is-invalid" : ""
+						}`}
+						type="text"
 						name="email"
 						id="email"
 						autoComplete="email"
+						{...register("email")}
 					/>
+					<div className="text-red-600">{errors.email?.message}</div>
 				</div>
 			</motion.div>
 			<motion.div className="flex flex-wrap mb-6 -mx-3" variants={fadeInUp}>
@@ -54,18 +87,22 @@ export default function ContactForm() {
 						Message
 					</label>
 					<textarea
-						className="block w-full h-48 px-4 py-3 mb-3 leading-tight text-gray-600 bg-white border border-pink-200 appearance-none resize-none rounded-xl no-resize focus:outline-none focus:bg-white focus:border-pink-500"
+						className={`block w-full h-48 px-4 py-3 mb-3 leading-tight text-gray-600 bg-white border border-pink-200 appearance-none resize-none rounded-xl no-resize focus:outline-none focus:bg-white focus:border-pink-500  ${
+							errors.message ? "is-invalid" : ""
+						}`}
 						id="message"
 						name="message"
+						{...register("message")}
 					></textarea>
+					<div className="text-red-600">{errors.message?.message}</div>
 				</div>
 			</motion.div>
 			<input
 				type="checkbox"
 				name="_honeypot"
 				style={{ display: "none" }}
-				tabindex="-1"
-				autocomplete="off"
+				tabIndex="-1"
+				autoComplete="off"
 			/>
 			<motion.div className="md:flex md:items-center" variants={fadeInUp}>
 				<div className="md:w-1/3">
