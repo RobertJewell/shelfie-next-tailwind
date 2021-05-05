e131f64c10000e73378e07602f7885424d9a2a0a
Robert Jewell <rjewellaudio@gmail.com>
2021-05-05 18:09:25 +0200

    add social links

---
+++ components/SocialLinks.js
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import InstagramIcon from "../public/images/icons/instagram.svg";
+import FacebookIcon from "../public/images/icons/facebook.svg";
+
+export default function SocialLinks() {
+	return (
+		<div>
+			<Link href="https://www.instagram.com/zjedzmy_troche_slodkosci/">
+				<a className="flex items-center my-6 transition-colors hover:text-pink-500">
+					<InstagramIcon className="h-6 fill-current"></InstagramIcon>
+					<p className="pl-4 text-sm">Instagram</p>
+				</a>
+			</Link>
+			<Link href="https://www.facebook.com/zjedzmytrocheslodkosci/">
+				<a className="flex items-center my-6 transition-colors hover:text-pink-500">
+					<FacebookIcon className="h-6 fill-current"></FacebookIcon>
+					<p className="pl-4 text-sm">Facebook</p>
+				</a>
+			</Link>
+		</div>
+	);
+}
