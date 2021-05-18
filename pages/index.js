dca1d667518fb3c43f7cc75dfef14700c62fb613
Robert Jewell <rjewellaudio@gmail.com>
2021-05-18 12:11:55 +0200

    initial index framer motion setup

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,39 @@
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
-
 import { motion } from "framer-motion";
 
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
 import ProductFeature from "../components/ProductFeature";
+
+const containerFade = {
+	initial: {
+		opacity: 0,
+	},
+	animate: {
+		y: 0,
+		opacity: 1,
+		transition: {
+			staggerChildren: 0.1,
+			delayChildren: 0.3,
+		},
+	},
+};
+
+const fadeInUp = {
+	initial: { y: 60, opacity: 0 },
+	animate: {
+		y: 0,
+		opacity: 1,
+	},
+};
+
+const fadeIn = {
+	initial: { opacity: 0 },
+	animate: { opacity: 1 },
+	transition: { duration: 0.8 },
+};
 
 export default function Home() {
 	return (
@@ -14,26 +41,40 @@ export default function Home() {
 			<NavBar></NavBar>
 			{/* landing */}
 			<section className="container flex justify-center px-6 mx-auto overflow-x-hidden sm:overflow-x-visible min-h-700 md:px-12 sm:min-h-400 snap-start">
-				<div className="relative flex flex-col items-center justify-around mx-auto sm:flex-row ">
+				<motion.div
+					className="relative flex flex-col items-center justify-around mx-auto sm:flex-row"
+					variants={containerFade}
+					initial="initial"
+					animate="animate"
+				>
 					{/* Text */}
 					<div className="z-10 w-full mt-16 text-center sm:w-1/2 sm:text-left sm:mt-0 sm:mb-12">
-						<h1 className="mx-auto text-5xl text-pink-500 sm:text-6xl max-w-4/5 sm:mx-0 sm:max-w-none">
+						<motion.h1
+							className="mx-auto text-5xl text-pink-500 sm:text-6xl max-w-4/5 sm:mx-0 sm:max-w-none"
+							variants={fadeInUp}
+						>
 							Zjedzmy trochę słodkości
-						</h1>
-						<h3 className="mt-6 text-sm text-gray-600 sm:text-md">
+						</motion.h1>
+						<motion.h3
+							className="mt-6 text-sm text-gray-600 sm:text-md"
+							variants={fadeInUp}
+						>
 							Pracownia cukiernicza
-						</h3>
-						<div className="mt-12">
+						</motion.h3>
+						<motion.div className="mt-12" variants={fadeInUp}>
 							<Link href="/contact" scroll={false}>
 								<a className="px-4 py-3 my-4 text-sm text-white transition-colors bg-pink-500 rounded-xl hover:bg-pink-600">
 									Złóż zamówienie
 								</a>
 							</Link>
-						</div>
+						</motion.div>
 					</div>
 					{/* Image */}
 					<div className="relative overflow-visible w-96 h-80 bottom-4 sm:w-96 sm:h-96">
-						<div className="absolute mx-auto -bottom-4 h-120 sm:h-full sm:right-0 w-140 -inset-x-1/4 sm:bottom-auto">
+						<motion.div
+							className="absolute mx-auto -bottom-4 h-120 sm:h-full sm:right-0 w-140 -inset-x-1/4 sm:bottom-auto"
+							variants={fadeIn}
+						>
 							<Image
 								className="object-contain"
 								src="/images/cakeFeature.png"
@@ -41,9 +82,9 @@ export default function Home() {
 								width={540}
 								height={390}
 							/>
-						</div>
+						</motion.div>
 					</div>
-				</div>
+				</motion.div>
 			</section>
 			{/* About */}
 			<section className="flex justify-center min-h-screen px-6 overflow-hidden bg-white sm:max-h-500 sm:min-h-400 md:px-12 snap-center">
@@ -57,7 +98,7 @@ export default function Home() {
 							height={256}
 						/>
 					</div>
-					<div className="px-6 pt-6 font-serif text-sm text-gray-600 sm:pt-0 md lg:pr-24">
+					<div className="px-6 pt-6 text-sm text-gray-600 sm:pt-0 md lg:pr-24">
 						<h3 className="hidden mb-4 text-xl font-bold text-pink-500 sm:block">
 							Hey!
 						</h3>
