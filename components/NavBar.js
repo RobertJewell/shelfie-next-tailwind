0e87c18258185f4a19ef543a55cbbceab326dc99
Robert Jewell <rjewellaudio@gmail.com>
2021-05-18 12:11:18 +0200

    start mobile navbar

--- components/NavBar.js
+++ components/NavBar.js
@@ -2,12 +2,19 @@ import React from "react";
 import NavBarMenu from "./NavBarMenu";
 import Image from "next/image";
 import Link from "next/link";
+import InstagramIcon from "../public/images/icons/instagram.svg";
+import FacebookIcon from "../public/images/icons/facebook.svg";
 
 const NavBar = () => {
 	return (
 		<div className="absolute top-0 hidden sm:relative sm:block ">
+			<div className="absolute hidden p-4 bg-white rounded-full">
+				<div className="w-6 h-1 mb-1.5 bg-pink-500 rounded-full "></div>
+				<div className="w-6 h-1 mb-1.5 bg-pink-500 rounded-full "></div>
+				<div className="w-6 h-1 bg-pink-500 rounded-full "></div>
+			</div>
 			<div className="container px-6 mx-auto font-bold text-gray-600 sm:px-6 md:px-12 ">
-				<div className="flex flex-row items-center justify-between h-16 mx-auto font-serif text-sm ">
+				<div className="flex flex-row items-center justify-between h-16 mx-auto font-sans text-sm ">
 					<Link href="/">
 						<a className="self-start">
 							<div className="h-12 overflow-hidden bg-pink-200 rounded-b-full ">
@@ -33,9 +40,18 @@ const NavBar = () => {
 							</Link>
 						</li>{" "}
 					</ul>
-					<Link href="/">
-						<a className="w-24 p-2 text-center">Social</a>
-					</Link>
+					<div className="flex flex-row justify-between w-24 p-2 text-center">
+						<Link href="https://www.instagram.com/zjedzmy_troche_slodkosci/">
+							<a className="p-4 my-6 transition-colors hover:text-pink-500">
+								<InstagramIcon className="h-6 fill-current"></InstagramIcon>
+							</a>
+						</Link>
+						<Link href="https://www.facebook.com/zjedzmytrocheslodkosci/">
+							<a className="p-4 my-6 transition-colors hover:text-pink-500">
+								<FacebookIcon className="h-6 fill-current"></FacebookIcon>
+							</a>
+						</Link>
+					</div>
 				</div>
 			</div>
 		</div>
